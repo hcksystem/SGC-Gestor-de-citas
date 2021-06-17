acad6379b8b156c7bb7493941b218f24f27c4a52
Yirlani González <66573042+yirlanigm@users.noreply.github.com>
2021-06-17 11:03:34 -0600

    Pagina maestra admin - css

---
+++ SGC Gestor de citas/frmMenuAdministrador.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SGC_Gestor_de_citas
+{
+    public partial class frmMenuAdministrador : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
