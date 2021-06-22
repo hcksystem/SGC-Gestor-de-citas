3c02ae70f4f15d400fdcc21c66f02238fe52688f
maikol95 <60193212+maikol95@users.noreply.github.com>
2021-06-21 19:28:50 -0600

    c2

---
+++ Entidades/Cita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cita
+    {
+        public int id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Hora { get; set; }
+        public bool Activo { get; set; }
+        public int idServicio { get; set; }
+        public int idUsuario { get; set; }
+
+    }
+}
---
+++ LogicaNegocio(BLL)/BLLCita.cs
@@ -0,0 +1,50 @@
+﻿using AccesoDatos_DAL_;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio_BLL_
+{
+    public class BLLCita
+    {
+        public void InsertarCita(string Descripcion, DateTime Fecha, DateTime Hora, bool Activo, int idServicio, int idUsuario)
+        {
+            DALCita daln = new DALCita();
+            daln.InsertarCita(Descripcion, Fecha, Hora, Activo, idServicio, idUsuario);
+        }
+        public void ModificarCita(int id,string Descripcion, DateTime Fecha, DateTime Hora, bool Activo, int idServicio, int idUsuario)
+        {
+            DALCita daln = new DALCita();
+            daln.ModificarCita(id,Descripcion,Fecha,Hora,Activo,idServicio,idUsuario);
+        }
+
+        public DataTable ObtenerTodaLasCitas()
+        {
+            DALCita daln = new DALCita();
+            return daln.ObtenerTodasLasCitas();
+        }
+
+        public DataTable ObtenerCitaPorID(int Identificacion)
+        {
+            DALCita daln = new DALCita();
+            return daln.ObtenerCitaPorID(Identificacion);
+        }
+
+        public void EliminarCita(int Identificacion)
+        {
+            DALCita daln = new DALCita();
+            daln.EliminarCita(Identificacion);
+        }
+
+        public void CancelarCita(int ID)
+        {
+            DALCita dalc = new DALCita();
+            dalc.CancelarCita(ID);
+        }
+
+
+    }
+}
